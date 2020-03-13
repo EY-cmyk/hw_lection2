41fcc16d0b0a55b71111701a0cc6d776037461d0
EY-cmyk <61290430+EY-cmyk@users.noreply.github.com>
2020-03-13 13:59:36 +0200

    Add files via upload

---
+++ hw_lection2_exercise2.py
@@ -0,0 +1,4 @@
+name= "Dima"
+sec_name = "Grechka"
+
+print(f"Hello {name} {sec_name},nice to meet you!")
---
+++ hw_lection2_exercise3.py
@@ -0,0 +1,8 @@
+import math
+a = 5
+b = 6
+addition = a + b
+subtraction = a - b 
+separation = a / b
+multipl = a  *  b
+print(f'''addition ={addition}, Subtraction = {subtraction}, Separation = {separation}, Multipl = {multipl}''')
